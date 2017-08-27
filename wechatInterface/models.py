23dd8a18180b8a3d2c6b0c2d9021d2ab7664d336
huanglijun <307045664@qq.com>
2017-08-28 01:51:05 +0800

    同步所有用户信息方法未完成，添加获取单个用户信息函数批量获取用户信息函数，优化http请求方法

--- wechatInterface/models.py
+++ wechatInterface/models.py
@@ -8,12 +8,9 @@ header_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0)
 
 def get_access_token(wechat_id):
     wechat = Subscription.objects.get(id=wechat_id)
-    get_parameter = {'appid': wechat.app_id, 'secret': wechat.app_secret}
-    get_parameter = parse.urlencode(get_parameter)
+    get_parameter = parse.urlencode({'appid': wechat.app_id, 'secret': wechat.app_secret})
     url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential'
-    response = request.Request(url='%s%s%s' % (url, '&', get_parameter), headers=header_dict)
-    result = request.urlopen(response)
-    result = loads(result.read().decode(encoding='utf-8'))
+    result = http_request(url='%s%s%s' % (url, '&', get_parameter))
     if 'access_token' in result:
         wechat.access_token = result['access_token']
         wechat.last_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -29,28 +26,38 @@ def get_all_fans(wechat_id):
         access_token = get_access_token(wechat_id)
     else:
         access_token = wechat.access_token
-    get_parameter = {'access_token': access_token}
-    get_parameter = parse.urlencode(get_parameter)
-    url = '%s%s%s' % ('https://api.weixin.qq.com/cgi-bin/user/get', '?', get_parameter)
-    response = request.Request(url=url, headers=header_dict)
-    result = request.urlopen(response)
-    fans = loads(result.read().decode(encoding='utf-8'))
     open_ids = []
+    get_parameter = parse.urlencode({'access_token': access_token})
+    url = '%s%s%s' % ('https://api.weixin.qq.com/cgi-bin/user/get', '?', get_parameter)
+    fans = http_request(url=url)
     open_ids.append(fans['data']['openid'])
     if fans['total'] > fans['count']:
         i = ceil(fans['total'] / fans['count'])
         for fetch in range(0, i):
-            get_parameter = {'next_openid': fans['next_openid']}
-            get_parameter = parse.urlencode(get_parameter)
-            url = '%s%s%s' % (url, '&', get_parameter)
-            response = request.Request(url=url, headers=header_dict)
-            result = request.urlopen(response)
-            fans = loads(result.read().decode(encoding='utf-8'))
+            get_parameter = parse.urlencode({'next_openid': fans['next_openid']})
+            fans = http_request(url='%s%s%s' % (url, '&', get_parameter))
             open_ids.append(fans['data']['openid'])
     for open_id in open_ids:
-        get_fans_data(open_id)
+        get_fans_data(open_id=open_id, access_token=access_token)
     return fans
 
 
-def get_fans_data(open_id):
-    return open_id
+def get_fans_data(open_id, access_token):
+    get_parameter = parse.urlencode({'access_token': access_token, 'openid': open_id, 'lang': 'zh_Cn'})
+    url = '%s%s%s' % ('https://api.weixin.qq.com/cgi-bin/user/info?', '&', get_parameter)
+    result = http_request(url=url)
+    return result
+
+
+def batch_get_fans_data(access_token):
+    get_parameter = parse.urlencode({'access_token': access_token})
+    url = '%s%s%s' % ('https://api.weixin.qq.com/cgi-bin/user/info/batchget', '?', get_parameter)
+    result = http_request(url=url)
+    return result
+
+
+def http_request(url):
+    response = request.Request(url=url, headers=header_dict)
+    result = request.urlopen(response)
+    result = loads(result.read().decode(encoding='utf-8'))
+    return result
